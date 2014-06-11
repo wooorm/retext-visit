193ba8e0b4264dd789423c498274bfde33522a8c
wooorm <tituswormer@gmail.com>
2014-06-11 17:40:33 +0200

    Unwrapped retextVisit from its IIFE; added strict mode

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-/* jshint -W084, -W093 */
-(function () {
-    exports = module.exports = function () {};
+'use strict';
 
-    exports.attach = attach;
+exports = module.exports = function () {};
 
-    function visit(callback) {
-        var node = this.head, next;
+function visit(callback) {
+    var node = this.head, next;
 
-        while (node) {
-            // Allow for removal of the node in the callback.
-            next = node.next;
+    while (node) {
+        // Allow for removal of the node in the callback.
+        next = node.next;
 
-            if (callback(node) === false) {
-                return;
-            }
-
-            (node.visit || visit).call(node, callback);
-
-            node = next;
+        if (callback(node) === false) {
+            return;
         }
-    }
 
-    function visitType(type, callback) {
-        var callbackWrapper = function (node) {
-            if (node.type === type) {
-                return callback(node);
-            }
-        };
-        this.visit.call(this, callbackWrapper);
-    }
+        (node.visit || visit).call(node, callback);
 
-    function attach(retext) {
-        var TextOM = retext.parser.TextOM,
-            parentPrototype = TextOM.Parent.prototype,
-            elementPrototype = TextOM.Element.prototype;
-
-        elementPrototype.visit = parentPrototype.visit = visit;
-        elementPrototype.visitType = parentPrototype.visitType = visitType;
+        node = next;
     }
-})();
+}
+
+function visitType(type, callback) {
+    var callbackWrapper = function (node) {
+        if (node.type === type) {
+            return callback(node);
+        }
+    };
+    this.visit.call(this, callbackWrapper);
+}
+
+function attach(retext) {
+    var TextOM = retext.parser.TextOM,
+        parentPrototype = TextOM.Parent.prototype,
+        elementPrototype = TextOM.Element.prototype;
+
+    elementPrototype.visit = parentPrototype.visit = visit;
+    elementPrototype.visitType = parentPrototype.visitType = visitType;
+}
+
+exports.attach = attach;
