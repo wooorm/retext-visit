93938d46b6ffa445ceceded92e15d421423400be
wooorm <tituswormer@gmail.com>
2014-09-22 11:21:08 +0200

    Refactor spec
    Includes a rewrite for retext ^0.2.0-rc.1

--- spec/retext-visit.spec.js
+++ spec/retext-visit.spec.js
@@ -1,93 +1,143 @@
 'use strict';
 
-var visit, TextOM, Retext, assert, retext, simpleWordNode;
+var visit,
+    Retext,
+    assert,
+    retext,
+    TextOM,
+    sentence;
+
+/**
+ * Module dependencies.
+ */
 
 visit = require('..');
 Retext = require('retext');
 assert = require('assert');
 
-retext = new Retext().use(visit).parse('A simple english sentence.');
+/**
+ * Constants.
+ */
+
+sentence = 'A simple english sentence.';
+
+retext = new Retext().use(visit);
 TextOM = retext.TextOM;
-simpleWordNode = retext.head.head.head.next.next;
+
+/**
+ * Unit tests.
+ */
 
 describe('visit', function () {
     it('should be of type `function`', function () {
         assert(typeof visit === 'function');
     });
 
-    it('should attach a `visit` method to `TextOM.Parent#`', function () {
-        assert('visit' in TextOM.Parent.prototype);
-        assert('visit' in TextOM.Element.prototype);
-        assert('visit' in TextOM.RootNode.prototype);
-        assert('visit' in TextOM.ParagraphNode.prototype);
-        assert('visit' in TextOM.SentenceNode.prototype);
-        assert(typeof retext.visit === 'function');
+    it('should attach a `visit` method to `Parent#`', function () {
+        assert(typeof (new TextOM.Parent()).visit === 'function');
+        assert(typeof (new TextOM.Element()).visit === 'function');
+        assert(typeof (new TextOM.RootNode()).visit === 'function');
+        assert(typeof (new TextOM.ParagraphNode()).visit === 'function');
+        assert(typeof (new TextOM.SentenceNode()).visit === 'function');
     });
 
-    it('should attach a `visitType` method to `TextOM.Parent#`', function () {
-        assert('visitType' in TextOM.Parent.prototype);
-        assert('visitType' in TextOM.Element.prototype);
-        assert('visitType' in TextOM.RootNode.prototype);
-        assert('visitType' in TextOM.ParagraphNode.prototype);
-        assert('visitType' in TextOM.SentenceNode.prototype);
-        assert(typeof retext.visitType === 'function');
+    it('should attach a `visitType` method to `Parent#`', function () {
+        assert(typeof (new TextOM.Parent()).visitType === 'function');
+        assert(typeof (new TextOM.Element()).visitType === 'function');
+        assert(typeof (new TextOM.RootNode()).visitType === 'function');
+        assert(typeof (new TextOM.ParagraphNode()).visitType === 'function');
+        assert(typeof (new TextOM.SentenceNode()).visitType === 'function');
     });
 });
 
+/**
+ * Unit tests for `visit`.
+ */
+
 describe('visit(callback)', function () {
-    it('should call callback for every node in the context',
-        function () {
-            var count = 0;
+    it('should invoke `callback` for every descendant of context',
+        function (done) {
+            retext.parse(sentence, function (err, tree) {
+                var count;
 
-            retext.visit(function () {
-                count++;
+                count = 0;
+
+                tree.visit(function () {
+                    count++;
+                });
+
+                assert(count === 18);
+
+                done(err);
             });
-
-            assert(count === 18);
         }
     );
 
-    it('should stop iterating when the callback returns false',
-        function () {
-            var count = 0;
+    it('should stop when `callback` returns `false`', function (done) {
+        retext.parse(sentence, function (err, tree) {
+            var count,
+                breakingNode;
 
-            retext.visit(function (node) {
+            count = 0;
+            breakingNode = tree.head.head.head.next.next;
+
+            tree.visit(function (node) {
                 count++;
-                if (node === simpleWordNode) {
+
+                if (node === breakingNode) {
                     return false;
                 }
             });
 
             assert(count === 7);
-        }
-    );
+
+            done(err);
+        });
+    });
 });
 
+/**
+ * Unit tests for `visitType`.
+ */
+
 describe('visitType(type, callback)', function () {
-    it('should call callback for every node of type `type`, in the context',
-        function () {
-            var count = 0;
+    it('should invoke `callback` for every descendant of context of `type`',
+        function (done) {
+            retext.parse(sentence, function (err, tree) {
+                var count;
 
-            retext.visitType(retext.WORD_NODE, function () {
-                count++;
+                count = 0;
+
+                tree.visitType(tree.WORD_NODE, function () {
+                    count++;
+                });
+
+                assert(count === 4);
+
+                done(err);
             });
-
-            assert(count === 4);
         }
     );
 
-    it('should stop iterating when the callback returns false',
-        function () {
-            var count = 0;
+    it('should stop when `callback` returns `false`', function (done) {
+        retext.parse(sentence, function (err, tree) {
+            var count,
+                breakingNode;
 
-            retext.visitType(retext.WORD_NODE, function (node) {
+            count = 0;
+            breakingNode = tree.head.head.head.next.next;
+
+            tree.visitType(tree.WORD_NODE, function (node) {
                 count++;
-                if (node === simpleWordNode) {
+
+                if (node === breakingNode) {
                     return false;
                 }
             });
 
             assert(count === 2);
-        }
-    );
+
+            done(err);
+        });
+    });
 });
